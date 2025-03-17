ed3a03a8bad6541e6f9760c78998f7ff5054671a
MhrrmliEltac <meherremlieltac14@gmail.com>
2025-03-17 18:20:48 +0400

    change api url

--- src/components/utils/Api.ts
+++ src/components/utils/Api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  // baseURL: "https://furniture-server-two.vercel.app/api/users",
-  baseURL: "http://localhost:4000/api/users",
+  baseURL: "https://furniture-server-two.vercel.app/api/users",
+  // baseURL: "http://localhost:4000/api/users",
   withCredentials: true,
 });
